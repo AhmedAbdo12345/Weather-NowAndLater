eadba8129fe12d4c2a736c76f99a014eb12f4850
AhmedAbdo12345 <Ahmedabdo12377@gmail.com>
2024-10-22 15:43:29 +0300

    Implement Weather Service Interface and Api Constant

---
+++ data/src/main/java/com/example/data/api/ApiConstants.kt
@@ -0,0 +1,8 @@
+package com.example.data.api
+
+object ApiConstants {
+    private const val API_PATH = "data/2.5"
+    const val API_PATH_WEATHER = "$API_PATH/weather"
+    const val API_PATH_FORECAST = "$API_PATH/forecast"
+
+}
---
+++ data/src/main/java/com/example/data/api/WeatherServices.kt
@@ -0,0 +1,21 @@
+package com.example.data.api
+
+import com.example.data.models.remote.CurrentResponse
+import com.example.data.models.remote.ForecastResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherServices {
+    @GET(ApiConstants.API_PATH_WEATHER)
+    suspend fun getCityWeather(
+        @Query("q") city: String,
+        @Query("appid") apiKey: String = ApiConstants.API_KEY
+    ): Response<CurrentResponse>
+
+    @GET(ApiConstants.API_PATH_FORECAST)
+    suspend fun getWeekForecast(
+        @Query("q") city: String,
+        @Query("appid") apiKey: String = ApiConstants.API_KEY
+    ): Response<ForecastResponse>
+}

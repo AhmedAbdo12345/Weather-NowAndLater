b12e4ab7be17989fd8c9d7d3775989f2c85c8107
Ahmed Abdo <50278028+AhmedAbdo12345@users.noreply.github.com>
2024-10-23 23:00:48 +0300

    Update CurrentWeatherScreen

--- features/current_weather/src/main/java/com/example/current_weather/ui/CurrentWeatherScreen.kt
+++ features/current_weather/src/main/java/com/example/current_weather/ui/CurrentWeatherScreen.kt
@@ -1,11 +1,14 @@
 package com.example.current_weather.ui
 
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,6 +16,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -23,6 +27,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -32,24 +37,32 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.core.util.WEEK_FORECAST
-import com.example.core.util.getDateTime
+import com.example.core.util.convertDtToDateTime
 import com.example.current_weather.R
-import com.example.weather_icon_library.getWeatherIcon
-import com.example.current_weather.viewmodel.HomeUiState
-import com.example.current_weather.viewmodel.CityWeatherViewModel
-import com.example.data.models.remote.CurrentResponse
 import com.example.current_weather.ui.theme.LiteBlue
 import com.example.current_weather.ui.theme.Yellow
+import com.example.current_weather.viewmodel.CityWeatherViewModel
+import com.example.current_weather.viewmodel.HomeUiState
+import com.example.data.models.remote.CurrentResponse
+import com.example.weather_icon_library.getWeatherIcon
 
 private const val TAG = "DisplayCurrentWeatherSc"
+
 @Composable
-fun CurrentWeatherScreen(viewModel: CityWeatherViewModel = hiltViewModel(), navController: NavHostController, cityName: String) {
+fun CurrentWeatherScreen(
+    viewModel: CityWeatherViewModel = hiltViewModel(),
+    navController: NavHostController,
+    cityName: String
+) {
     val homeUiState by viewModel.homeUiState.collectAsState()
     LaunchedEffect(Unit) {
         viewModel.getCityWeather(cityName)
     }
     when (homeUiState) {
-        is HomeUiState.Loading -> {}
+        is HomeUiState.Loading -> {
+            CircularProgressBar()
+        }
+
         is HomeUiState.Success -> {
             val currentResponse = (homeUiState as? HomeUiState.Success)?.currentResponse
             Log.d(TAG, "HomeScreen: $currentResponse")
@@ -61,6 +74,7 @@ fun CurrentWeatherScreen(viewModel: CityWeatherViewModel = hiltViewModel(), navC
         is HomeUiState.Failed -> {
             val errorMessage = (homeUiState as? HomeUiState.Failed)?.message
             Log.d(TAG, "HomeScreenError: $errorMessage")
+            Toast.makeText(LocalContext.current, errorMessage, Toast.LENGTH_SHORT).show()
 
         }
     }
@@ -69,7 +83,21 @@ fun CurrentWeatherScreen(viewModel: CityWeatherViewModel = hiltViewModel(), navC
 }
 
 @Composable
-fun DisplayData(currentResponse: CurrentResponse,navController: NavHostController, cityName: String) {
+fun CircularProgressBar() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun DisplayData(
+    currentResponse: CurrentResponse,
+    navController: NavHostController,
+    cityName: String
+) {
     Column {
 
         Column(
@@ -81,7 +109,7 @@ fun DisplayData(currentResponse: CurrentResponse,navController: NavHostControlle
             Text(text = currentResponse.name.orEmpty(), fontSize = 24.sp)
             Spacer(modifier = Modifier.height(8.dp))
             currentResponse.dt?.let {
-             Text(text = getDateTime(it), fontSize = 16.sp)
+                Text(text = convertDtToDateTime(it), fontSize = 16.sp)
             }
             Spacer(modifier = Modifier.height(16.dp))
             Icon(
@@ -99,8 +127,13 @@ fun DisplayData(currentResponse: CurrentResponse,navController: NavHostControlle
 
         }
         WeatherDashboard(currentResponse)
-        Button(onClick = { navController.navigate("$WEEK_FORECAST/$cityName")}) {
-            Text(text = "Show Weekly Forecast", fontSize = 14.sp)
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(onClick = { navController.navigate("$WEEK_FORECAST/$cityName") }) {
+                Text(text = "Show Weekly Forecast", fontSize = 14.sp)
+            }
         }
     }
 
